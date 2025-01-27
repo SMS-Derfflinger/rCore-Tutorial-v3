0dc6d414461c9072e54ea4eafb57762e7d328bdf
Heinz <aurelianob808@gmail.com>
2025-01-27 23:24:05 +0800

    finish ch1 book exercise

--- myos/src/main.rs
+++ myos/src/main.rs
@@ -3,6 +3,8 @@
 #![feature(panic_info_message)]
 mod lang_items;
 mod sbi;
+mod log;
+use ::log::*;
 
 #[macro_use]
 mod console;
@@ -12,9 +14,29 @@ global_asm!(include_str!("entry.asm"));
 
 #[no_mangle]
 pub fn rust_main() -> ! {
+    extern "C" {
+        fn stext(); // begin addr of text segment
+        fn etext(); // end addr of text segment
+        fn srodata(); // start addr of Read-Only data segment
+        fn erodata(); // end addr of Read-Only data ssegment
+        fn sdata(); // start addr of data segment
+        fn edata(); // end addr of data segment
+        fn sbss(); // start addr of BSS segment
+        fn ebss(); // end addr of BSS segment
+        fn boot_stack_lower_bound(); // stack lower bound
+        fn boot_stack_top(); // stack top
+    }
+
     clear_bss();
-    println!("Hello, world!");
-    panic!("Shutdown machine!");
+    log::init();
+    println!("[kernel] Hello, world!");
+    trace!("[kernel] .text [{:#x}, {:#x})", stext as usize, etext as usize);
+    debug!("[kernel] .rodata [{:#x}, {:#x})", srodata as usize, erodata as usize);
+    info!("[kernel] .data [{:#x}, {:#x})", sdata as usize, edata as usize);
+    warn!("[kernel] boot_stack top=bottom={:#x}, lower_bound={:#x}", boot_stack_top as usize, boot_stack_lower_bound as usize);
+    error!("[kernel] .bss [{:#x}, {:#x})", sbss as usize, ebss as usize);
+
+    sbi::shutdown(false)
 }
 
 fn clear_bss() {
